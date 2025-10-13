ae7b1c6dc010eec9582081e55835974cc7badf08
Dhruv Yadav <150121335+dhruvyadavv@users.noreply.github.com>
2025-10-13 10:10:11 +0530

    Time: 1 ms (99.78%) | Memory: 44.9 MB (33.97%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,21 +1,21 @@
 class Solution {
-    public List<String> removeAnagrams(String[] words) {
-        Stack<String> stack = new Stack<>();
-        for(int i = words.length-1;i>= 0;i--){
-            String s = words[i];
-            while(!stack.isEmpty() && anagram(stack.peek(),s) == true)stack.pop();
-            stack.push(s);
-        }
-        List<String> res = new ArrayList<>();
-        while(!stack.isEmpty())res.add(stack.pop());
-        return res;
+    public List<String> removeAnagrams(String[] A) {
+    List<String> ans= new ArrayList<>();
+    int n= A.length;
+    for(int i=0;i<n;){
+        int j=i+1;
+        while( j<n && isang(A[i],A[j]) ) j++;
+        ans.add(A[i]);
+        i=j;
     }
-    
-    public boolean anagram(String p,String q){
-        int arr[] = new int[26];
-        for(char i : p.toCharArray())arr[i-'a']+=1;
-        for(char i : q.toCharArray())arr[i-'a']-=1;
-        for(int i : arr)if(i != 0)return false;
-        return true;
-    }
+    return ans;
+}
+boolean isang(String p,String q){
+    int[] cnt= new int[26];
+    int res=0;
+    for(int i=0;i<p.length();i++) cnt[p.charAt(i)-'a']++;
+    for(int i=0;i<q.length();i++) cnt[q.charAt(i)-'a']--;
+    for(int i:cnt) if(i!=0) return false;
+    return true;
+}
 }
