07e2ec1e5d29b70554a5b5d4034443c63a4c3d52
Dhruv Yadav <150121335+dhruvyadavv@users.noreply.github.com>
2025-10-13 10:08:07 +0530

    Time: 3 ms (55.46%) | Memory: 44.7 MB (82.79%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,16 +1,21 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        String prev ="";
-        List<String> li=new ArrayList<>();
-        for(int i=0;i<words.length;i++){
-            char[] ch=words[i].toCharArray();
-            Arrays.sort(ch);
-            String curr=String.valueOf(ch);
-            if(!curr.equals(prev)){
-                li.add(words[i]);
-                prev=curr;
-            }
+        Stack<String> stack = new Stack<>();
+        for(int i = words.length-1;i>= 0;i--){
+            String s = words[i];
+            while(!stack.isEmpty() && anagram(stack.peek(),s) == true)stack.pop();
+            stack.push(s);
         }
-        return li;
+        List<String> res = new ArrayList<>();
+        while(!stack.isEmpty())res.add(stack.pop());
+        return res;
+    }
+    
+    public boolean anagram(String p,String q){
+        int arr[] = new int[26];
+        for(char i : p.toCharArray())arr[i-'a']+=1;
+        for(char i : q.toCharArray())arr[i-'a']-=1;
+        for(int i : arr)if(i != 0)return false;
+        return true;
     }
 }
